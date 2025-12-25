7b825d54160aca2e60035ec6f3fcd329c372f0a2
Ahmad Alfin Nur Hakim <ahmadalfin130804@gmail.com>
2025-12-26 01:38:03 +0700

    Fix OpenCV Python version issue

--- app.py
+++ app.py
@@ -21,15 +21,15 @@ if "last_detection_time" not in st.session_state:
 # =========================
 #   PAGE UI
 # =========================
-st.title("🤟 Sign Language Recognition - Streamlit")
-st.write("Sign Language Detection using YOLO (Image Upload Mode)")
+st.title("🤟 Sign Language Recognition")
+st.write("Upload image of hand sign to detect letters")
 
 # ---------- TEXT AREA ----------
 st.subheader("📌 Detected Text")
 st.write(st.session_state.detected_text)
 
 # ---------- BUTTONS ----------
-col1, col2, col3, col4, col5 = st.columns(5)
+col1, col2, col3, col4 = st.columns(4)
 
 if col1.button("Space"):
     st.session_state.detected_text += " "
@@ -40,12 +40,7 @@ if col2.button("Delete"):
 if col3.button("Clear All"):
     st.session_state.detected_text = ""
 
-if col4.button("Save to TXT"):
-    with open("detected_text.txt", "w") as f:
-        f.write(st.session_state.detected_text)
-    st.success("Saved as detected_text.txt 👍")
-
-col5.download_button(
+col4.download_button(
     label="Download TXT",
     data=st.session_state.detected_text,
     file_name="detected_text.txt",
